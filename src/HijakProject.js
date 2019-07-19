9c4c17aa6afae49a52bc54e2996e1e6f0a26804c
Allain Lalonde <alalonde@elastalink.com>
2019-07-19 17:39:09 -0400

    Adding HijakProject.install/uninstall

--- src/HijakProject.js
+++ src/HijakProject.js
@@ -1,4 +1,9 @@
+import { loadJson, loadJsonSync, saveJson } from "./lib/load-file"
+import Debug from "debug"
+import fs from "fs-extra"
 import path from "path"
+
+const debug = Debug("hijak:install")
 
 export default class HijakProject {
   constructor(projectDir) {
@@ -6,14 +11,43 @@ export default class HijakProject {
   }
 
   get installed() {
-    return false
+    return !!loadJsonSync(path.join(this.projectDir, "package.json")).hijak
   }
 
   get buildPath() {
     return path.join(this.projectDir, "node_modules", ".hijak")
   }
 
-  async install(gitUrl) {}
+  async _updatePkg(fn) {
+    const pkgJsonPath = path.resolve(this.projectDir, "package.json")
+    const pkg = await loadJson(pkgJsonPath)
+    const newPkg = fn(pkg) || pkg
+    await saveJson(pkgJsonPath, newPkg)
+  }
 
-  async uninstall() {}
+  async _loadProjectPackage() {
+    const pkgJsonPath = path.resolve(this.projectDir, "package.json")
+    return await loadJson(pkgJsonPath)
+  }
+
+  async install(gitUrl) {
+    debug("adding hijack config into package.json")
+    await this._updatePkg(pkg => ({ ...pkg, hijak: { repo: gitUrl } }))
+
+    if (fs.pathExists(this.buildPath)) {
+      debug("removing %s", this.buildPath)
+      await fs.remove(this.buildPath)
+    }
+  }
+
+  async uninstall() {
+    if (fs.pathExists(this.buildPath)) {
+      debug("removing %s", this.buildPath)
+      await fs.remove(this.buildPath)
+    }
+
+    await this._updatePkg(pkg => {
+      delete pkg.hijak
+    })
+  }
 }
